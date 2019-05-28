2d45d3a8e26a9d3c6f87b114b45f6b72c06e3702
atom3 <artomczak@gmail.com>
2019-05-28 19:39:40 +0200

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,31 @@
+# Tokens below should match the ones stated on Facebook Messenger Developers website:
+"""
+THOSE TOKENS WERE GENERATED FOR THE TestBot4316
+"""
+fb_app_id = "295745724625491"
+fb_app_secret = "3188052c0da8abac95c1c10713d14034"
+fb_client = "fe7a99d3ac128e2986da4bac56c1272a"
+fb_access = 'EAAEMZBqrcMlMBAAbnNpK8OBlRJsZBlOnNfjtWU6EjJHzZBrOY23mqn9ZBNlM3LqPzr1ZBuaGrDfZBouRx4HWNGLy10llWmGpGtTUxhLAd68DZCojk9DmK96GKqoUWqyFZC1KCsC5NNgz2Kao0ZCNTsWfZAVBoxGZCm7LPXoreeMP8WcnwZDZD'
+fb_verification = 'rAnd0mpassssw2130210349421'   #haslo podane na fb dev w sekcji webhooks
+
+#FB User IDs:
+# TODO czy to potrzebne?
+fb_bot_id = '371953230018877'
+fb_artur_id = '2046412982111085'
+fb_kuba_id = '2410700988971224'
+
+# WitAI data:
+# TODO wklej dobre tokeny
+wit_app_name = "RockPaperScissors"
+wit_app_id = "2463402677009365"
+wit_server_access = "QRQH7JMNWKGP6WY6RA4FUOVDRQF4FNOP"
+wit_client_access = "6TKH47DLHJAGNDGZB2R6GJDOFMA4NI5V"
+
+# MySQL DB connection data:
+# TODO wklej dobre tokeny
+sql_config = {
+    'user': 'root',
+    'password': """Ad4Gw2""",
+    'host': 'localhost',
+    'raise_on_warnings': True
+}
