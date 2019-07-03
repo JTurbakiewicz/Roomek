4b5169bcbfdf95c19904fa9b48902a4cae6686db
Tyb3r <43152548+Tyb3r@users.noreply.github.com>
2019-07-03 19:36:51 +0200

    Database update.
    Signed-off-by: Tyb3r <43152548+Tyb3r@users.noreply.github.com>

--- Databases/mysql_connection.py
+++ Databases/mysql_connection.py
@@ -6,7 +6,7 @@ import os
 import tokens
 import sys
 from Bot.user import User
-from Bot.message_parser import Message
+from Bot.message import Message
 
 # logging.basicConfig(level='DEBUG')
 """Funtion definition"""
@@ -267,6 +267,7 @@ def get_user(facebook_id):
                  """ % (facebook_id)
         cursor.execute(query)
         data = cursor.fetchone()
+
         created_user = User(data['facebook_id'])
 
         created_user.first_name = data['first_name']
@@ -456,27 +457,21 @@ def create_message(msg_obj = None, update = False):
         msg_obj = Message('default')
 
     with DB_Connection(db_config, DB_NAME) as (cnx, cursor):
-        fields_to_add = 'id'
-        msg_data = [msg_obj.id]
+        fields_to_add = 'messaging'
+        msg_data = [str(msg_obj.id)]
 
-        if msg_obj.messaging:
-            msg_data.append(str(msg_obj.messaging))
-            fields_to_add = fields_to_add + ',messaging'
+        if msg_obj.is_echo:
+            msg_data.append(msg_obj.is_echo)
+            fields_to_add = fields_to_add + ',is_echo'
         if msg_obj.time:
             msg_data.append(msg_obj.time)
             fields_to_add = fields_to_add + ',time'
         if msg_obj.timestamp:
             msg_data.append(msg_obj.timestamp)
             fields_to_add = fields_to_add + ',timestamp'
-        if msg_obj.senderID:
-            msg_data.append(str(msg_obj.senderID))
-            fields_to_add = fields_to_add + ',senderID'
-        if msg_obj.recipientID:
-            msg_data.append(str(msg_obj.recipientID))
-            fields_to_add = fields_to_add + ',recipientID'
-        if msg_obj.user:
-            msg_data.append(str(msg_obj.user.facebook_id))
-            fields_to_add = fields_to_add + ',facebook_id'
+        if msg_obj.user_id:
+            msg_data.append(str(msg_obj.user.user_id))
+            fields_to_add = fields_to_add + ',user_id'
         if msg_obj.type:
             msg_data.append(str(msg_obj.type))
             fields_to_add = fields_to_add + ',type'
@@ -513,14 +508,6 @@ def create_message(msg_obj = None, update = False):
         if msg_obj.NLP_intent:
             msg_data.append(str(msg_obj.NLP_intent))
             fields_to_add = fields_to_add + ',NLP_intent'
-        if msg_obj.sender:
-            msg_data.append(str(msg_obj.sender))
-            fields_to_add = fields_to_add + ',sender'
-        if msg_obj.recipient:
-            msg_data.append(str(msg_obj.recipient))
-            fields_to_add = fields_to_add + ',recipient'
-
-
 
         placeholders = '%s,' * len(fields_to_add.split(','))
         placeholders = placeholders[:-1]
@@ -547,12 +534,12 @@ def create_message(msg_obj = None, update = False):
         cnx.commit()
 
 
-def get_messages(id):
+def get_messages(user_id):
     with DB_Connection(db_config, DB_NAME) as (cnx, cursor):
         query =  """SELECT *
                  FROM conversations
-                 WHERE id = %s
-                 """ % (id)
+                 WHERE user_id = %s
+                 """ % (user_id)
         cursor.execute(query)
         data = cursor.fetchall()
 
@@ -583,13 +570,11 @@ def get_messages(id):
 
         for message in data:
             created_message = Message(fake_json)
-            created_message.id = message['id']
+            created_message.is_echo = message['is_echo']
             created_message.messaging = message['messaging']
             created_message.time = message['time']
             created_message.timestamp = message['timestamp']
-            created_message.senderID = message['senderID']
-            created_message.recipientID = message['recipientID']
-            created_message.user = message['facebook_id']
+            created_message.user = message['user_id']
             created_message.type = message['type']
             created_message.mid = message['mid']
             created_message.NLP = message['NLP']
@@ -602,8 +587,7 @@ def get_messages(id):
             created_message.NLP_entities = message['NLP_entities']
             created_message.NLP_language = message['NLP_language']
             created_message.NLP_intent = message['NLP_intent']
-            created_message.sender = message['sender']
-            created_message.recipient = message['recipient']
+
 
             created_messages.append(created_message)
 
@@ -767,15 +751,13 @@ db_tables['users'] = (
 db_tables['conversations'] = (
     "CREATE TABLE `conversations` ("
     "  `conversation_no` int(1) NOT NULL AUTO_INCREMENT,"
-    "  `id` char(100) NOT NULL,"
+    "  `is_echo` BOOLEAN,"
     "  `messaging` varchar(5000),"   
     "  `time` date,"
     "  `timestamp` date," 
-    "  `senderID` char(100),"
-    "  `recipientID` char(100),"
-    "  `facebook_id` char(100),"
+    "  `user_id` char(100),"
     "  `type` char(100),"
-    "  `mid` int(1),"
+    "  `mid` varchar(1000),"
     "  `NLP` BOOLEAN,"
     "  `stickerID` varchar(999),"
     "  `sticker_name` varchar(999),"
@@ -786,8 +768,6 @@ db_tables['conversations'] = (
     "  `NLP_entities` varchar(999),"
     "  `NLP_language` varchar(999),"
     "  `NLP_intent` varchar(999),"
-    "  `sender` varchar(99),"
-    "  `recipient` varchar(99),"
     "  PRIMARY KEY (`conversation_no`)"
     ") ENGINE=InnoDB")
 
@@ -863,117 +843,117 @@ db_config = tokens.sql_config
 
 set_up_db(db_config)
 
-# json_text = {'entry': [{
-#    'id': '1368143226655403',
-#    'messaging': [{
-#       'message': {
-#          'mid': 'GZ1aEkkMhti8WBa22tk5lXWJoZXN9QKZWH8NjK5DYEFKf7dFmM-QZEUThzNoJk73q2QSR5AD_aEDnRjNS6XHbw',
-#          'nlp': {
-#             'entities': {
-#                'bye': [{
-#                   '_entity': 'bye',
-#                   'confidence': 0.29891659254789,
-#                   'value': 'true'}],
-#                'greetings': [{
-#                   '_entity': 'greetings',
-#                   'confidence': 0.99988770484656,
-#                   'value': 'true'}],
-#                'sentiment': [{
-#                   '_entity': 'sentiment',
-#                   'confidence': 0.54351806476846,
-#                   'value': 'positive'},
-#                   {'_entity': 'sentiment',
-#                   'confidence': 0.43419978802319,
-#                   'value': 'neutral'},
-#                   {'_entity': 'sentiment',
-#                   'confidence': 0.022282101881069,
-#                   'value': 'negative'}],
-#                'thanks': [{
-#                   '_entity': 'thanks',
-#                   'confidence': 0.056984366913182,
-#                   'value': 'true'}]}},
-#          'seq': 671186,
-#          'text': 'hello'},
-#    'recipient': {'id': '1368143226655403'},
-#    'sender': {'id': '2231584683532589'},
-#    'timestamp': 1550245454526}],
-#    'time': 1550245455532}],
-# 'object': 'page'}
-#
-# json_sticker = {'entry': [{
-#    'id': '1368143226655403',
-#    'messaging': [{
-#       'message': {
-#          'attachments': [{
-#             'payload': {
-#                'sticker_id': 139084406650204,
-#                'url': 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/18601789_139084409983537_1112176674583085056_n.png?_nc_cat=1&_nc_ad=z-m&_nc_cid=0&_nc_zor=9&_nc_ht=scontent.xx&oh=58dcff07f7efab2581eb2bf46ad09b69&oe=5D2622E6'},
-#                'type': 'image'}],
-#          'mid': 'vTPBDAg3IFSvojFSoA-hc3WJoZXN9QKZWH8NjK5DYEFtpDoGWGtpfyyd39iwwpoX3UKTT03qWPoLw9dKCPmgbw',
-#          'seq': 671191,
-#          'sticker_id': 139084406650204},
-#       'recipient': {'id': '1368143226655403'},
-#       'sender': {'id': '2231584683532589'},
-#       'timestamp': 1550245813786}],
-#    'time': 1550245814781}],
-# 'object': 'page'}
-#
-# json_location = {'object': 'page', 'entry': [{'id': '371953230018877', 'time': 1555281043795, 'messaging': [
-#     {'sender': {'id': '2046412982111085'}, 'recipient': {'id': '371953230018877'}, 'timestamp': 1555264002482,
-#      'message': {'mid': 'DdF-MlFpNZDrbxkE68kCNYqbfPNS9lyKlT61---k1D4QCj3DOAeMtT6yyYUviIHA8ZlIA71-vkrb-uv2Muqrlg',
-#                  'seq': 711130, 'attachments': [{'title': "Artur's Location",
-#                                                  'url': 'https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.bing.com%2Fmaps%2Fdefault.aspx%3Fv%3D2%26pc%3DFACEBK%26mid%3D8100%26where1%3D52.221069523573%252C%2B21.005859375%26FORM%3DFBKPL1%26mkt%3Den-US&h=AT2Zyxelz8ZK0Jod6SDT6I-lXNGPzxzAYTLNBg7Z2JjiclGERs1k9Jk5kAiHCRVF6BUb_DWtLJ0NNIKsxlXOvpv6RlxFUIlAWahFnFbURhiVRM3VOY61-RVHcWRNLO44rsUqqdtalhLl&s=1',
-#                                                  'type': 'location', 'payload': {
-#                  'coordinates': {'lat': 52.221069523573, 'long': 21.005859375}}}]}}]}]}
-#
-# json_wit = {'object': 'page', 'entry':
-# [{'id': '371953230018877',
-# 'time': 1553348850822,
-# 'messaging': [{
-#    'sender': {'id': '2046412982111085'},
-#    'recipient': {'id': '371953230018877'},
-#    'timestamp': 1553348849836,
-#    'message':
-#       {'mid': 'qN8omcX5vWrOSUaepj5gnIqbfPNS9lyKlT61---k1D59qd3_oMN-A9tKNZRmLp5MEnhAoyCvgAtS7ql06IMSsQ',
-#       'seq': 697389,
-#       'text': 'szukam mieszkania w okolicy',
-#       'nlp': {
-#          'entities':
-#             {'housing_type': [{'confidence': 0.99150295898905, 'value': 'apartment', 'type': 'value', '_entity': 'housing_type', '_body': 'mieszkania', '_start': 7, '_end': 17}],
-#              'location': [{'suggested': True, 'confidence': 0.83922, 'value': 'okolicy', 'type': 'value', '_entity': 'location', '_body': 'okolicy', '_start': 20, '_end': 27}],
-#              'intent': [{'confidence': 0.99914076780032, 'value': 'looking for', '_entity': 'intent'}]}}}}]}]}
-#
-# json_confirmation = {'entry': [{
-#    'id': '1368143226655403',
-#    'messaging': [{
-#       'delivery': {
-#          'mids': [
-#             'fJo_skAtj0UAj5GgomOw53WJoZXN9QKZWH8NjK5DYEE9B5dnUilQaMt-2X-DZRfZPmQnzikom5h1pXysIono0w',
-#             'THFi2alVD8V3n83TUhy5IXWJoZXN9QKZWH8NjK5DYEHQCUziMeG424BvnntyRSwsrCFBtvlVEcJc2g3akNjrsg',
-#             'a0jKJ7wGYETe-n5xjhOK-nWJoZXN9QKZWH8NjK5DYEGO9a4neOVsmrjMyO4OkNUliCY0OnwVakSKkc1C2_c55g'],
-#          'seq': 0,
-#          'watermark': 1550245456341},
-#       'recipient': {'id': '1368143226655403'},
-#       'sender': {'id': '2231584683532589'},
-#       'timestamp': 1550245459149}],
-#    'time': 1550245459170}],
-# 'object': 'page'}
-#
-# create_user(facebook_id='1', last_name='Kuba', update=True)
-# msg = Message(json_text)
-# create_message(msg_obj=msg)
-#
-# msg = Message(json_sticker)
-# create_message(msg_obj=msg)
-#
-# msg = Message(json_location)
-# create_message(msg_obj=msg)
-#
-# msg = Message(json_wit)
-# create_message(msg_obj=msg)
-#
-# msg = Message(json_confirmation)
-# create_message(msg_obj=msg)
-#
-# a = get_messages('1368143226655403')
+json_text = {'entry': [{
+   'id': '1368143226655403',
+   'messaging': [{
+      'message': {
+         'mid': 'GZ1aEkkMhti8WBa22tk5lXWJoZXN9QKZWH8NjK5DYEFKf7dFmM-QZEUThzNoJk73q2QSR5AD_aEDnRjNS6XHbw',
+         'nlp': {
+            'entities': {
+               'bye': [{
+                  '_entity': 'bye',
+                  'confidence': 0.29891659254789,
+                  'value': 'true'}],
+               'greetings': [{
+                  '_entity': 'greetings',
+                  'confidence': 0.99988770484656,
+                  'value': 'true'}],
+               'sentiment': [{
+                  '_entity': 'sentiment',
+                  'confidence': 0.54351806476846,
+                  'value': 'positive'},
+                  {'_entity': 'sentiment',
+                  'confidence': 0.43419978802319,
+                  'value': 'neutral'},
+                  {'_entity': 'sentiment',
+                  'confidence': 0.022282101881069,
+                  'value': 'negative'}],
+               'thanks': [{
+                  '_entity': 'thanks',
+                  'confidence': 0.056984366913182,
+                  'value': 'true'}]}},
+         'seq': 671186,
+         'text': 'hello'},
+   'recipient': {'id': '1368143226655403'},
+   'sender': {'id': '2231584683532589'},
+   'timestamp': 1550245454526}],
+   'time': 1550245455532}],
+'object': 'page'}
+
+json_sticker = {'entry': [{
+   'id': '1368143226655403',
+   'messaging': [{
+      'message': {
+         'attachments': [{
+            'payload': {
+               'sticker_id': 139084406650204,
+               'url': 'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/18601789_139084409983537_1112176674583085056_n.png?_nc_cat=1&_nc_ad=z-m&_nc_cid=0&_nc_zor=9&_nc_ht=scontent.xx&oh=58dcff07f7efab2581eb2bf46ad09b69&oe=5D2622E6'},
+               'type': 'image'}],
+         'mid': 'vTPBDAg3IFSvojFSoA-hc3WJoZXN9QKZWH8NjK5DYEFtpDoGWGtpfyyd39iwwpoX3UKTT03qWPoLw9dKCPmgbw',
+         'seq': 671191,
+         'sticker_id': 139084406650204},
+      'recipient': {'id': '1368143226655403'},
+      'sender': {'id': '2231584683532589'},
+      'timestamp': 1550245813786}],
+   'time': 1550245814781}],
+'object': 'page'}
+
+json_location = {'object': 'page', 'entry': [{'id': '371953230018877', 'time': 1555281043795, 'messaging': [
+    {'sender': {'id': '2046412982111085'}, 'recipient': {'id': '371953230018877'}, 'timestamp': 1555264002482,
+     'message': {'mid': 'DdF-MlFpNZDrbxkE68kCNYqbfPNS9lyKlT61---k1D4QCj3DOAeMtT6yyYUviIHA8ZlIA71-vkrb-uv2Muqrlg',
+                 'seq': 711130, 'attachments': [{'title': "Artur's Location",
+                                                 'url': 'https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.bing.com%2Fmaps%2Fdefault.aspx%3Fv%3D2%26pc%3DFACEBK%26mid%3D8100%26where1%3D52.221069523573%252C%2B21.005859375%26FORM%3DFBKPL1%26mkt%3Den-US&h=AT2Zyxelz8ZK0Jod6SDT6I-lXNGPzxzAYTLNBg7Z2JjiclGERs1k9Jk5kAiHCRVF6BUb_DWtLJ0NNIKsxlXOvpv6RlxFUIlAWahFnFbURhiVRM3VOY61-RVHcWRNLO44rsUqqdtalhLl&s=1',
+                                                 'type': 'location', 'payload': {
+                 'coordinates': {'lat': 52.221069523573, 'long': 21.005859375}}}]}}]}]}
+
+json_wit = {'object': 'page', 'entry':
+[{'id': '371953230018877',
+'time': 1553348850822,
+'messaging': [{
+   'sender': {'id': '2046412982111085'},
+   'recipient': {'id': '371953230018877'},
+   'timestamp': 1553348849836,
+   'message':
+      {'mid': 'qN8omcX5vWrOSUaepj5gnIqbfPNS9lyKlT61---k1D59qd3_oMN-A9tKNZRmLp5MEnhAoyCvgAtS7ql06IMSsQ',
+      'seq': 697389,
+      'text': 'szukam mieszkania w okolicy',
+      'nlp': {
+         'entities':
+            {'housing_type': [{'confidence': 0.99150295898905, 'value': 'apartment', 'type': 'value', '_entity': 'housing_type', '_body': 'mieszkania', '_start': 7, '_end': 17}],
+             'location': [{'suggested': True, 'confidence': 0.83922, 'value': 'okolicy', 'type': 'value', '_entity': 'location', '_body': 'okolicy', '_start': 20, '_end': 27}],
+             'intent': [{'confidence': 0.99914076780032, 'value': 'looking for', '_entity': 'intent'}]}}}}]}]}
+
+json_confirmation = {'entry': [{
+   'id': '1368143226655403',
+   'messaging': [{
+      'delivery': {
+         'mids': [
+            'fJo_skAtj0UAj5GgomOw53WJoZXN9QKZWH8NjK5DYEE9B5dnUilQaMt-2X-DZRfZPmQnzikom5h1pXysIono0w',
+            'THFi2alVD8V3n83TUhy5IXWJoZXN9QKZWH8NjK5DYEHQCUziMeG424BvnntyRSwsrCFBtvlVEcJc2g3akNjrsg',
+            'a0jKJ7wGYETe-n5xjhOK-nWJoZXN9QKZWH8NjK5DYEGO9a4neOVsmrjMyO4OkNUliCY0OnwVakSKkc1C2_c55g'],
+         'seq': 0,
+         'watermark': 1550245456341},
+      'recipient': {'id': '1368143226655403'},
+      'sender': {'id': '2231584683532589'},
+      'timestamp': 1550245459149}],
+   'time': 1550245459170}],
+'object': 'page'}
+
+create_user(facebook_id='1', last_name='Kuba', update=True)
+msg = Message(json_text)
+create_message(msg_obj=msg)
+
+msg = Message(json_sticker)
+create_message(msg_obj=msg)
+
+msg = Message(json_location)
+create_message(msg_obj=msg)
+
+msg = Message(json_wit)
+create_message(msg_obj=msg)
+
+msg = Message(json_confirmation)
+create_message(msg_obj=msg)
+
+a = get_messages('1368143226655403')
 
